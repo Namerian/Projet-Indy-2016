3eb2c5ab4fec6c2be8a21e5a1bd0cd805e4b3efe
Unknown <patrickmonville@hotmail.com>
2016-11-10 16:25:15 +0100

    changed stuff for a test

--- Assets/Scripts/Game/GameController.cs
+++ Assets/Scripts/Game/GameController.cs
@@ -4,63 +4,63 @@ using System;
 
 public class GameController : MonoBehaviour, IMissionListener
 {
-    private PlayerController redPlayer;
-    private PlayerController greenPlayer;
-    private PlayerController bluePlayer;
-    private PlayerController yellowPlayer;
+	private PlayerController redPlayer;
+	private PlayerController greenPlayer;
+	private PlayerController bluePlayer;
+	private PlayerController yellowPlayer;
 
-    private GameObject missionViewObject;
+	private GameObject missionViewObject;
 
-    private Mission currentMission;
+	private Mission currentMission;
 
-    // Use this for initialization
-    void Start()
-    {
-        //UI
-        missionViewObject = GameObject.Find("UI/InGameUI/MissionUI/MissionTimerPanel");
+	void Awake ()
+	{
+		//UI
+		missionViewObject = GameObject.Find ("UI/InGameUI/MissionUI/MissionTimerPanel");
 
-        //Players
-        redPlayer = GameObject.Find("PlayerHolder/RedPlayer").GetComponent<PlayerController>();
-        greenPlayer = GameObject.Find("PlayerHolder/GreenPlayer").GetComponent<PlayerController>();
-        bluePlayer = GameObject.Find("PlayerHolder/BluePlayer").GetComponent<PlayerController>();
-        yellowPlayer = GameObject.Find("PlayerHolder/YellowPlayer").GetComponent<PlayerController>();
+		//Players
+		redPlayer = GameObject.Find ("PlayerHolder/RedPlayer").GetComponent<PlayerController> ();
+		greenPlayer = GameObject.Find ("PlayerHolder/GreenPlayer").GetComponent<PlayerController> ();
+		bluePlayer = GameObject.Find ("PlayerHolder/BluePlayer").GetComponent<PlayerController> ();
+		yellowPlayer = GameObject.Find ("PlayerHolder/YellowPlayer").GetComponent<PlayerController> ();
+	}
 
-        redPlayer.Initialize(0);
-        greenPlayer.Initialize(1);
-        bluePlayer.Initialize(2);
-        yellowPlayer.Initialize(3);
-    }
+	// Use this for initialization
+	void Start ()
+	{
+		redPlayer.Initialize (0);
+		greenPlayer.Initialize (1);
+		bluePlayer.Initialize (2);
+		yellowPlayer.Initialize (3);
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (currentMission == null && Input.GetKeyDown(KeyCode.E))
-        {
-            currentMission = new Mission();
-            currentMission.AddListener(this);
-            missionViewObject.GetComponent<MissionTimerPanelView>().Initialize(currentMission);
-            currentMission.Start();
-        }
-        else if (currentMission != null)
-        {
-            currentMission.Update();
-        }
-    }
+	// Update is called once per frame
+	void Update ()
+	{
+		if (currentMission == null && Input.GetKeyDown (KeyCode.E)) {
+			currentMission = new Mission ();
+			currentMission.AddListener (this);
+			missionViewObject.GetComponent<MissionTimerPanelView> ().Initialize (currentMission);
+			currentMission.Start ();
+		} else if (currentMission != null) {
+			currentMission.Update ();
+		}
+	}
 
-    //==========================================================================================================
-    // IMissionListener Implementation
-    //==========================================================================================================
+	//==========================================================================================================
+	// IMissionListener Implementation
+	//==========================================================================================================
 
-    void IMissionListener.OnMissionStarted(Mission mission)
-    {
-    }
+	void IMissionListener.OnMissionStarted (Mission mission)
+	{
+	}
 
-    void IMissionListener.OnTimerUpdated(Mission mission)
-    {
-    }
+	void IMissionListener.OnTimerUpdated (Mission mission)
+	{
+	}
 
-    void IMissionListener.OnMissionEnded(Mission mission)
-    {
-        currentMission = null;
-    }
+	void IMissionListener.OnMissionEnded (Mission mission)
+	{
+		currentMission = null;
+	}
 }
